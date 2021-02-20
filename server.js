a561116dc5e540446d91225a7eb83fa61a9ad50b
Anna Kijania <kijania.anna@gmail.com>
2021-02-20 18:50:28 +0100

    Add contact form

---
+++ server.js
@@ -0,0 +1,49 @@
+const cors = require('cors');
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+
+const creds = require('./config');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+app.listen(5000, () => console.log('Server Running'));
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS,
+  },
+});
+
+contactEmail.verify(error => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Ready to Send');
+  }
+});
+
+router.post('/contact', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: name,
+    to: creds.USER,
+    subject: 'Contact Form Submission',
+    html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, error => {
+    if (error) {
+      res.json({ status: 'ERROR' });
+    } else {
+      res.json({ status: 'Message Sent' });
+    }
+  });
+});
--- src/components/views/About/About.js
+++ src/components/views/About/About.js
@@ -7,73 +7,104 @@ import {
   faSass,
 } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-
-import PageNotFound from '../PageNotFound/PageNotFound';
+import React, { useState } from 'react';
 
 import styles from './About.module.scss';
 
-const About = ({ error }) => {
-  if (error) return <PageNotFound />;
-  else
-    return (
-      <div className='container'>
-        <div className='row'>
-          <div className={styles.topControlbar}>
-            <h2>About me</h2>
-          </div>
+const About = () => {
+  const [status, setStatus] = useState('Submit');
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setStatus('Sending...');
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch('http://localhost:5000/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus('Submit');
+    let result = await response.json();
+    alert(result.status);
+  };
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className={styles.topControlbar}>
+          <h2>About me</h2>
         </div>
-        <div className='row'>
-          <div className='col-7'>
-            <p>
-              My name is Anna Kijania. A year and a half ago, I made a decision to
-              change the industry. My dream was to become a Web Developer. At the
-              beginning, I was learning independently from the Udemy and FreeCodeCamp
-              courses. This, however, was not enough for me. In May, I started a 9-month
-              Kodilla course. I had a meeting with a mentor every week. I learned React,
-              Redux, JavaScript, Node.js, Express, MongoDB{' '}
-            </p>
-            <div className={styles.skills}>
-              <h4>Skills</h4>
-              <ul>
-                <li>
-                  <FontAwesomeIcon className={styles.icon} icon={faHtml5} /> HTML/{' '}
-                  <FontAwesomeIcon className={styles.icon} icon={faSass} /> Sass
-                </li>
-                <li>
-                  <FontAwesomeIcon className={styles.icon} icon={faJs} /> JavaScript
-                </li>
-                <li>
-                  <FontAwesomeIcon className={styles.icon} icon={faBootstrap} />{' '}
-                  Bootstrap
-                </li>
-                <li>
-                  <FontAwesomeIcon className={styles.icon} icon={faReact} /> React/
-                  Redux
-                </li>
-                <li>
-                  <FontAwesomeIcon className={styles.icon} icon={faNode} /> Node.js/
-                  Express.js
-                </li>
-                <li>MongoDB/ Mongoose</li>
-                <li>Photoshop/ CorelDRAW</li>
-              </ul>
-            </div>
-            <h4>Hobby</h4>
-            <p>traveling, architecture, dance</p>
+      </div>
+      <div className='row'>
+        <div className='col-xs-12 col-lg-7'>
+          <p>
+            My name is Anna Kijania. A year and a half ago, I made a decision to change
+            the industry. My dream was to become a Web Developer. At the beginning, I
+            was learning independently from the Udemy and FreeCodeCamp courses. This,
+            however, was not enough for me. In May, I started a 9-month Kodilla course.
+            I had a meeting with a mentor every week. I learned React, Redux,
+            JavaScript, Node.js, Express, MongoDB
+          </p>
+          <div className={styles.skills}>
+            <h4>Skills</h4>
+            <ul>
+              <li>
+                <FontAwesomeIcon className={styles.icon} icon={faHtml5} /> HTML/
+                <FontAwesomeIcon className={styles.icon} icon={faSass} /> Sass
+              </li>
+              <li>
+                <FontAwesomeIcon className={styles.icon} icon={faJs} /> JavaScript
+              </li>
+              <li>
+                <FontAwesomeIcon className={styles.icon} icon={faBootstrap} />
+                Bootstrap
+              </li>
+              <li>
+                <FontAwesomeIcon className={styles.icon} icon={faReact} /> React/ Redux
+              </li>
+              <li>
+                <FontAwesomeIcon className={styles.icon} icon={faNode} /> Node.js/
+                Express.js
+              </li>
+              <li>MongoDB/ Mongoose</li>
+              <li>Photoshop/ CorelDRAW</li>
+            </ul>
           </div>
-          <div className='col-5'>
-            <div className={styles.box}>
-              <div className={styles.boxContent}>
-                <h4>Contact me</h4>
-                <p>mail: kijania.anna@gmail.com</p>
-                <p>phone: +48 509095907</p>
+          <h4>Hobby</h4>
+          <p>traveling, architecture, dance</p>
+        </div>
+        <div className='col-xs-12 col-lg-5'>
+          <div className={styles.box}>
+            <h4>Contact me</h4>
+            <p>phone: +48 509095907</p>
+            <p>e-mail: kijania.anna@gmail.com</p>
+            <form onSubmit={handleSubmit}>
+              <div className='form-group'>
+                <label htmlFor='name'>Name:</label>
+                <input className='form-control' type='text' id='name' required />
               </div>
-            </div>
+              <div className='form-group'>
+                <label htmlFor='email'>Email:</label>
+                <input className='form-control' type='email' id='email' required />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='message'>Message:</label>
+                <textarea className='form-control' id='message' required />
+              </div>
+              <button type='submit' className='btn btn-primary'>
+                {status}
+              </button>
+            </form>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default About;
